eef8351aaadfd23bb9638e811c339534f44a35fc
The-Matheus <matheusaguiar18@outlook.com>
2022-01-31 14:49:28 -0300

    Desafio de lógica 05 - Estrutura de dados complexos (Array)

---
+++ script.js
@@ -0,0 +1,31 @@
+// Crie um array que receba 5 itens e exiba no console.
+
+let cores = ["branco","amarelo pastel","salmão","rosa","preto"]
+console.log(cores)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+cores.unshift("verde")
+console.log(cores)
+
+// Utilize um método para remover o último nome do array.
+
+cores.pop()
+console.log(cores)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+cores.push("vermelho", "bege")
+console.log(cores)
+
+// Utilize um método para remover o primeiro nome do array.
+
+cores.shift()
+console.log(cores)
+
+// Utilize um método para organizar em ordem crescente o seguinte array: 
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers.sort())
+
+
